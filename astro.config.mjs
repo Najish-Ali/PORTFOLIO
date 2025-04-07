2e42d82ec2c9c08a66713e589932916d2fb00478
Najish Ali <sayyednajishali@gmail.com>
2025-04-07 14:39:58 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
+
+// export default defineConfig({
+//   base: '/PORTFOLIO/',
+//   // output: 'static', // Required for GitHub Pages deployment
+// });
+
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  base: '/PORTFOLIO/',
-  // output: 'static', // Required for GitHub Pages deployment
+  integrations: [tailwind()],
 });
